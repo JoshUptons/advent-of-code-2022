62d159d38503e2aeb8cf62210f336f82aa944487
JoshUptons <itsjoshupton@gmail.com>
2022-12-02 09:59:55 -0800

    solution part 2

--- day1/main.go
+++ day1/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
+
+func sortHeap(heap []int) {
+	sort.Slice(heap, func(i, j int) bool {
+		return heap[i] > heap[j]
+	})
+}
 
 func main() {
 
@@ -14,14 +21,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	highCalories := 0
+	highestCals := make([]int, 3)
 
 	currentElf := 0
 
 	for _, line := range strings.Split(string(file), "\n") {
 		if line == "" || line == "\n" {
-			if currentElf > highCalories {
-				highCalories = currentElf
+			if currentElf > highestCals[len(highestCals)-1] {
+				highestCals[2] = currentElf
+				sortHeap(highestCals)
 			}
 			currentElf = 0
 		}
@@ -29,6 +37,11 @@ func main() {
 		currentElf += cals
 	}
 
-	log.Println(highCalories)
+	totalCals := 0
+	for _, cals := range highestCals {
+		totalCals += cals
+	}
+
+	log.Println(totalCals)
 
 }
